c95cf5f996c3b4f72803fe6c47dc7ebee05b2d5f
Cristian <146520221+CristianCretti@users.noreply.github.com>
2023-10-10 12:49:16 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,7 +1,11 @@
-CREATE TABLE cliente (
-	CF int PRIMARY KEY,
-	Nome varchar(50) NOT NULL,
-	Cognome varchar(50) NOT NULL,
-	Tipo varchar(50) NOT NULL
+CREATE TABLE Forma(
+    CodForma int PRIMARY KEY,
+    Stagionatura varchar(30) NOT NULL,
+    Scelta varchar(20) NOT NULL,
+    Np int NOT NULL,
+    Data date NOT NULL,
+    CodCas int,
+    CF int(16),
+    FOREIGN KEY (CodCas) REFERENCES Caseificio(CodCas),
+    FOREIGN KEY (CF) REFERENCES Cliente(CF)
 );
-
