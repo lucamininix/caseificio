bdcacd72fbce7420ef3d8af1adbe311b1c4f476d
CRETTI <giulia.cretti@pianadid.local>
2023-10-10 12:19:13 +0200

    aggiunte longitudine e latitudine

--- db.sql
+++ db.sql
@@ -3,11 +3,13 @@ CREATE TABLE utenti (
 );
 
 create table  caseificio (
-	Codcaseificio int PRIMARY KEY 
-	Nome  varchar (50) NOT NULL
-	NomeTitolare varchar(50) NOT NULL
-	Indirizzo varchar(100) NOT NULL
-	Citta varchar(50) NOT NULL
-	CAP int NOT NULL
-	Provincia varchar(50) NOT NULL
+	Codcaseificio int PRIMARY KEY,
+	Nome  varchar (50) NOT NULL,
+	NomeTitolare varchar(50) NOT NULL,
+	Indirizzo varchar(100) NOT NULL,
+	Citta varchar(50) NOT NULL,
+	CAP int NOT NULL,
+	Provincia varchar(50) NOT NULL,
+	Latitudine int not null,
+	Longitudine int not null
 );
