9572a3f771b066d7a580de3b66fc3ce590bc3c36
lucamininix <146519524+lucamininix@users.noreply.github.com>
2023-10-13 15:54:40 +0000

    Fixed inconsistency problems

--- db.sql
+++ db.sql
@@ -5,26 +5,26 @@ CREATE TABLE cliente (
     Tipo varchar(50) NOT NULL
 );
 
-CREATE TABLE forma(
+CREATE TABLE forma (
     CodForma int PRIMARY KEY,
     Stagionatura varchar(30) NOT NULL,
     Scelta varchar(20) NOT NULL,
     Numero int NOT NULL,
     Data date NOT NULL,
-    CodCas int,
-    CF int(16),
-    FOREIGN KEY (CodCas) REFERENCES caseificio(CodCas),
+    CodCaseificio int,
+    CF varchar(16),
+    FOREIGN KEY (CodCaseificio) REFERENCES caseificio(CodCaseificio),
     FOREIGN KEY (CF) REFERENCES cliente(CF)
 );
 
 CREATE TABLE caseificio (
-	CodCaseificio int PRIMARY KEY,
-	Nome  varchar (50) NOT NULL,
-	NomeTitolare varchar(50) NOT NULL,
-	Indirizzo varchar(100) NOT NULL,
-	Citta varchar(50) NOT NULL,
-	CAP int NOT NULL,
-	Provincia varchar(50) NOT NULL,
-	Latitudine int NOT NULL,
-	Longitudine int NOT NULL
+    CodCaseificio int PRIMARY KEY,
+    Nome  varchar (50) NOT NULL,
+    NomeTitolare varchar(50) NOT NULL,
+    Indirizzo varchar(100) NOT NULL,
+    Citta varchar(50) NOT NULL,
+    CAP int NOT NULL,
+    Provincia varchar(50) NOT NULL,
+    Latitudine int NOT NULL,
+    Longitudine int NOT NULL
 );
