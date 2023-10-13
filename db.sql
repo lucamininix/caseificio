9b6ab25ad14ca41de5e0ab55a8e23a03bfbbefe2
lucamininix <146519524+lucamininix@users.noreply.github.com>
2023-10-13 15:42:05 +0000

    Fixed client which had been erased

--- db.sql
+++ db.sql
@@ -1,5 +1,6 @@
-CREATE TABLE utenti (
-	id int PRIMARY KEY
+CREATE TABLE cliente (
+    CF varchar(16) PRIMARY KEY,
+    Nome varchar(50) NOT NULL,
+	Cognome varchar(50) NOT NULL,
+	Tipo varchar(50) NOT NULL
 );
-
-
