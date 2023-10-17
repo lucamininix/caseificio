15891cf5d33f981022e52cc994995487a02bab10
Matteobenti <146823840+Matteobenti@users.noreply.github.com>
2023-10-17 12:33:07 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -13,8 +13,8 @@ CREATE TABLE caseificio (
 CREATE TABLE raccolta (
     Data date,
     CodCaseificio int,
-    LatteRaccolto varchar(55) NOT NULL,
-    LatteLavorato varchar(55) NOT NULL,
+    LatteRaccolto int NOT NULL,
+    LatteLavorato int NOT NULL,
     PRIMARY KEY (Data,CodCaseificio),
     FOREIGN KEY (CodCaseificio) REFERENCES Caseificio (CodCaseificio)
 );
